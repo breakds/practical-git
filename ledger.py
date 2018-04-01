14344bef7acd2aff0c6e38811b9fb8f260e2ce7d
Break.Yang <yiqing.yang@jingchi.ai>
2018-03-31 17:03:38 -0700

    2018-01-04, Red Packet to Miao

--- ledger.py
+++ ledger.py
@@ -4,6 +4,9 @@ def RunMyLedger():
     # 2018-01-01, Red Packet from Tony
     balance += 3.0
 
+    # 2018-01-04, Red Packet to Miao
+    balance -= 50.0
+
     print("My total balance: {}".format(balance))
 
 if __name__ == "__main__":
