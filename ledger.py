ae777802805af1320eec177bb04bada279fd63de
Break.Yang <yiqing.yang@jingchi.ai>
2018-03-31 17:54:18 -0700

    2018-01-3, (Wife) Win Texas Holder in Las Vegas

--- ledger.py
+++ ledger.py
@@ -4,6 +4,9 @@ def RunMyLedger():
     # 2018-01-01, Red Packet from Tony
     balance += 3.0
 
+    # 2018-01-03, (Wife) Win Texas Holder in Las Vegas
+    balance += 75.0
+
     print("My total balance: {}".format(balance))
 
 if __name__ == "__main__":
